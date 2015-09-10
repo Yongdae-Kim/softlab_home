5d5d9221f019f1c060eb9c13218194a08484ff35
daum <daum@aptest.local>
2015-09-10 18:10:15 +0900

    bootstrap 파일 수정

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,73 +5,45 @@ class BootStrap {
 
 	def init = { servletContext ->
 
-//		new User(userId : 'test@hanmail.net', userPw : "123", userRole : UserRole.admin).save()
-		
-//		new Researcher(admissionNum : '1900', name : "강경옥", phoneNum : "01030805770").save()
-//
-//		new Researcher(admissionNum : '1900', name : "고승용", phoneNum : "01076398051").save()
-//
-//		new Researcher(admissionNum : '1900', name : "김정철", phoneNum : "01039300130").save()
-//
-//		new Researcher(admissionNum : '1900', name : "김현준", phoneNum : "01096872790" ).save()
-//
-//		new Researcher(admissionNum : '1900', name : "김희삼", phoneNum : "01071796166" ).save()
-//
-//		new Researcher(admissionNum : '1900', name : "남현욱", phoneNum : "01036395125" ).save()
-//
-//		new Researcher(admissionNum : '1900', name : "송창용", phoneNum : "01049116575" ).save()
-//
-//		new Researcher(admissionNum : '1900', name : "장우성", phoneNum : "01026879440" ).save()
-//
-//		new Researcher(admissionNum : '1900', name : "정효원", phoneNum : "01086619739" ).save()
-//
-//		new Researcher(admissionNum : '1900', name : "이정하", phoneNum : "01026443121" ).save()
-//
-//		new Researcher(admissionNum : '1900', name : "정은경", phoneNum : "01098385242" ).save()
-//
-//		new Researcher(admissionNum : '1900', name : "오은희", phoneNum : "0116393009" ).save()
-//
-//		new Researcher(admissionNum : '2002', name : "김여종", phoneNum : "01034875111" ).save()
-//
-//		new Researcher(admissionNum : '2002', name : "정세철", phoneNum : "01063084940" ).save()
-//
-//		new Researcher(admissionNum : '2002', name : "여경철", phoneNum : "01066935663" ).save()
-//
-//		new Researcher(admissionNum : '2004', name : "오윤덕", phoneNum : "01098396673" ).save()
-//
-//		new Researcher(admissionNum : '2005', name : "고시철", phoneNum : "01037209877" ).save()
-//
-//		new Researcher(admissionNum : '2005', name : "이혜진", phoneNum : "01099897589" ).save()
-//
-//		new Researcher(admissionNum : '2006', name : "강우진", phoneNum : "01067892730" ).save()
-//
-//		new Researcher(admissionNum : '2006', name : "오도연", phoneNum : "01048752912" ).save()
-//
-//		new Researcher(admissionNum : '2007', name : "오대헌", phoneNum : "01026892368" ).save()
-//
-//		new Researcher(admissionNum : '2010', name : "송나은", phoneNum : "01026403932" ).save()
-//
-//		new Researcher(admissionNum : '2008', name : "현광민", phoneNum : "01048472221", email : 'kmin00h@gmail.com').save()
-//
-//		new Researcher(admissionNum : '2008', name : "이상준", phoneNum : "01028581624", email : 'zzazngnau@naver.com').save()
-//
-//		new Researcher(admissionNum : '2008', name : "김수민", phoneNum : "01034008365", email : 'alphamale405@gmail.com').save()
-//		
-//		new Researcher(admissionNum : '2009', name : "김용대", phoneNum : "01099417258", email : 'scratchback91@gmail.com' ).save()
-//
-//		new Researcher(admissionNum : '2010', name : "한경종", phoneNum : "01095025222", email : 'valunas713@gmail.com').save()
-//
-//		new Researcher(admissionNum : '2011', name : "신영재", phoneNum : "01030140804", email : 'feverneva@gmail.com' ).save()
-//
-//		new Researcher(admissionNum : '2011', name : "김준호", phoneNum : "01076254005", email : 'kjh44004@naver.com').save()
-//
-//		new Researcher(admissionNum : '2014', name : "김아람", phoneNum : "01075864999", email : 'theclouu@gmail.com' ).save()
-//
-//		new Researcher(admissionNum : '2009', name : "김민석", phoneNum : "01071925315", email : 'rhfcjdgur@naver.com').save()
-//
-//		new Researcher(admissionNum : '2014', name : "강임선", phoneNum : "01064210400", email : 'dlatjs4380@naver.com').save()
+		//		new User(userId : 'admin@softlab.ac.kr', userPw : "123", userRole : UserRole.admin).save()
+		//		new User(userId : 'java@softlab.ac.kr', userPw : "123", userRole : UserRole.java).save()
+		//		new User(userId : 'cs@softlab.ac.kr', userPw : "123", userRole : UserRole.csharp).save()
+		//		new User(userId : 'and@softlab.ac.kr', userPw : "123", userRole : UserRole.android).save()
+		//		new User(userId : 'data@softlab.ac.kr', userPw : "123", userRole : UserRole.dataStr).save()
+
+		//		new Researcher(admissionNum : '1900', name : "강경옥", phoneNum : "01030805770").save()
+		//		new Researcher(admissionNum : '1900', name : "고승용", phoneNum : "01076398051").save()
+		//		new Researcher(admissionNum : '1900', name : "김정철", phoneNum : "01039300130").save()
+		//		new Researcher(admissionNum : '1900', name : "김현준", phoneNum : "01096872790" ).save()
+		//		new Researcher(admissionNum : '1900', name : "김희삼", phoneNum : "01071796166" ).save()
+		//		new Researcher(admissionNum : '1900', name : "남현욱", phoneNum : "01036395125" ).save()
+		//		new Researcher(admissionNum : '1900', name : "송창용", phoneNum : "01049116575" ).save()
+		//		new Researcher(admissionNum : '1900', name : "장우성", phoneNum : "01026879440" ).save()
+		//		new Researcher(admissionNum : '1900', name : "정효원", phoneNum : "01086619739" ).save()
+		//		new Researcher(admissionNum : '1900', name : "이정하", phoneNum : "01026443121" ).save()
+		//		new Researcher(admissionNum : '1900', name : "정은경", phoneNum : "01098385242" ).save()
+		//		new Researcher(admissionNum : '1900', name : "오은희", phoneNum : "0116393009" ).save()
+		//		new Researcher(admissionNum : '2002', name : "김여종", phoneNum : "01034875111" ).save()
+		//		new Researcher(admissionNum : '2002', name : "정세철", phoneNum : "01063084940" ).save()
+		//		new Researcher(admissionNum : '2002', name : "여경철", phoneNum : "01066935663" ).save()
+		//		new Researcher(admissionNum : '2004', name : "오윤덕", phoneNum : "01098396673" ).save()
+		//		new Researcher(admissionNum : '2005', name : "고시철", phoneNum : "01037209877" ).save()
+		//		new Researcher(admissionNum : '2005', name : "이혜진", phoneNum : "01099897589" ).save()
+		//		new Researcher(admissionNum : '2006', name : "강우진", phoneNum : "01067892730" ).save()
+		//		new Researcher(admissionNum : '2006', name : "오도연", phoneNum : "01048752912" ).save()
+		//		new Researcher(admissionNum : '2007', name : "오대헌", phoneNum : "01026892368" ).save()
+		//		new Researcher(admissionNum : '2010', name : "송나은", phoneNum : "01026403932" ).save()
+		//		new Researcher(admissionNum : '2008', name : "현광민", phoneNum : "01048472221", email : 'kmin00h@gmail.com').save()
+		//		new Researcher(admissionNum : '2008', name : "이상준", phoneNum : "01028581624", email : 'zzazngnau@naver.com').save()
+		//		new Researcher(admissionNum : '2008', name : "김수민", phoneNum : "01034008365", email : 'alphamale405@gmail.com').save()
+		//		new Researcher(admissionNum : '2009', name : "김용대", phoneNum : "01099417258", email : 'scratchback91@gmail.com' ).save()
+		//		new Researcher(admissionNum : '2010', name : "한경종", phoneNum : "01095025222", email : 'valunas713@gmail.com').save()
+		//		new Researcher(admissionNum : '2011', name : "신영재", phoneNum : "01030140804", email : 'feverneva@gmail.com' ).save()
+		//		new Researcher(admissionNum : '2011', name : "김준호", phoneNum : "01076254005", email : 'kjh44004@naver.com').save()
+		//		new Researcher(admissionNum : '2014', name : "김아람", phoneNum : "01075864999", email : 'theclouu@gmail.com' ).save()
+		//		new Researcher(admissionNum : '2009', name : "김민석", phoneNum : "01071925315", email : 'rhfcjdgur@naver.com').save()
+		//		new Researcher(admissionNum : '2014', name : "강임선", phoneNum : "01064210400", email : 'dlatjs4380@naver.com').save()
 	}
 	def destroy = {
-
 	}
 }
